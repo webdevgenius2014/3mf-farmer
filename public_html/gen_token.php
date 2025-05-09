a96523857c467f6cd960c3b0038feff3d2be7b6d
88tajsingh <88tajsingh@gmail.com>
2025-05-09 17:40:45 +0530

    addig the admi mail where token has beeen sent over mail

--- public_html/gen_token.php
+++ public_html/gen_token.php
@@ -3,7 +3,7 @@ $token = bin2hex(random_bytes(4)); // 8-character token
 file_put_contents(__DIR__ . '/../token.txt', $token);
 
 // Optional: email it
-// $to = "client@example.com";
-// $subject = "New Access Token for Loop Modifier";
-// $message = "Your new token is: $token\n\nURL: https://loop.3d-farmers.com?token=$token";
-// mail($to, $subject, $message);
+$to = "contact@3d-farmers.com";
+$subject = "New Access Token for Loop Modifier";
+$message = "Your new token is: $token\n\nURL: https://loop.3d-farmers.com?token=$token";
+mail($to, $subject, $message);
